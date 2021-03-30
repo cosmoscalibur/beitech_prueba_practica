1af5eeb92b47302997141c686376cea0dfdd8cdf
Edward Villegas-Pulgarin <cosmoscalibur@gmail.com>
2021-03-30 08:37:16 -0500

    Se incorporan métodos de inserción y actualización de la base de datos para la creación de órdenes.

--- beitech_app/database.py
+++ beitech_app/database.py
@@ -26,6 +26,21 @@ class dbmanager:
         except psycopg2.Error as e:
             print(e)
 
+    def insert(self, query: str):
+        try:
+            self.cursor.execute(query)
+            res = self.cursor.fetchall()[0][0]
+            return res
+        except psycopg2.Error as e:
+            print(e)
+    
+    def call(self, proc_with_params:str):
+        try:
+            self.cursor.execute(f"call {proc_with_params};")
+            res = self.cursor.fetchall()
+            return res
+        except psycopg2.Error as e:
+            print(e)
 
 
 dbobj = dbmanager()
@@ -40,13 +55,15 @@ def get_products():
 
 
 def get_customer_products(customer_id):
-    return dbobj.get_json(f"""
+    return dbobj.get_json(
+        f"""
     select cp.product_id, pr.name
     from public.customer_product cp
     left join public.product pr
     on cp.product_id = pr.product_id
     where cp.customer_id = {customer_id}
-    """)
+    """
+    )
 
 
 def get_customer_orders(customer_id: int, bdate: str, edate: str):
@@ -55,7 +72,7 @@ def get_customer_orders(customer_id: int, bdate: str, edate: str):
     )
 
 
-def query_insert_single(tablename: str, colnames: list, values: list):
+def query_insert_single(tablename: str, colnames: list, values: list, returning: str):
     colnames = ", ".join(colnames)
     values = ", ".join(
         [f"'{value}'" if isinstance(value, str) else f"{value}" for value in values]
@@ -63,4 +80,28 @@ def query_insert_single(tablename: str, colnames: list, values: list):
     return f"""
     INSERT INTO {tablename} ({colnames})
     VALUES ({values})
+    RETURNING {returning};\n
     """
+
+
+def query_create_order(order_detail: list):
+    order_id = dbobj.insert(
+        query_insert_single(
+            "public.order",
+            ["customer_id", "delivery_address", "creation_date"],
+            [order_detail[0], order_detail[1], order_detail[2]],
+            "order_id"
+        )
+    )
+    for product_quantity in order_detail[3]:
+        order_detail_id = dbobj.insert(
+            query_insert_single(
+                "public.order_detail",
+                ["order_id", "product_id", "quantity"],
+                [order_id, product_quantity[0], product_quantity[1]],
+                "order_detail_id"
+            )
+        )
+    dbobj.call(f"public.update_order_detail({order_id})")
+    dbobj.connection.commit()
+    return order_id
