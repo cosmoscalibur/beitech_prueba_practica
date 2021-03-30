11bdb7c39631b9616cf2e6cb021e33ca1691fcf6
Edward Villegas-Pulgarin <cosmoscalibur@gmail.com>
2021-03-30 10:14:06 -0500

    Corrección a los campos numéricos de ingreso, para restringir valores ingresados y forzar que sean numéricos. Se cambia a campo html5 para esto

--- beitech_app/forms.py
+++ beitech_app/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
-from wtforms.fields.html5 import DateField
-from wtforms import SubmitField, SelectField, IntegerField, StringField
+from wtforms.fields.html5 import DateField, IntegerField
+from wtforms.widgets.html5 import NumberInput
+from wtforms import SubmitField, SelectField, StringField
 from wtforms.validators import InputRequired, NumberRange, Length
 from pathlib import Path
 import datetime
@@ -32,13 +33,13 @@ class OrdersForm(FlaskForm):
         "Fecha inicial",
         [InputRequired(message="Se requiere de una fecha inicial de búsqueda")],
         format="%Y-%m-%d",
-        default=bdate_default
+        default=bdate_default,
     )
     edate = DateField(
         "Fecha final",
         [InputRequired(message="Se requiere de una fecha final de búsqueda")],
         format="%Y-%m-%d",
-        default=edate_default
+        default=edate_default,
     )
     submit = SubmitField("Consultar")
 
@@ -50,54 +51,73 @@ class CreateForm(FlaskForm):
         [InputRequired(message="Se requiere ingresar el cliente")],
         coerce=int,
         choices=[(item["customer_id"], item["name"]) for item in customers],
-        default=1
+        default=1,
     )
     creation_date = DateField(
         "Fecha de creación",
         [InputRequired(message="Se requiere ingresar fecha de creación")],
         format="%Y-%m-%d",
-        default=datetime.date.today()
+        default=datetime.date.today(),
     )
 
-    delivery_address = StringField("Dirección de entrega", [InputRequired(message="Se requiere dirección de entrega"), Length(max=191)])
+    delivery_address = StringField(
+        "Dirección de entrega",
+        [InputRequired(message="Se requiere dirección de entrega"), Length(max=191)],
+    )
 
-    pr_choices = [(item["product_id"], item["name"]) for item in dbman.get_customer_products(1)]
+    pr_choices = [
+        (item["product_id"], item["name"]) for item in dbman.get_customer_products(1)
+    ]
 
     product_id_1 = SelectField(
         "Producto 1",
         [InputRequired(message="Se requiere al menos un producto")],
-        coerce=int, choices=pr_choices
+        coerce=int,
+        choices=pr_choices,
     )
     quantity_1 = IntegerField(
         "Cantidad 1",
         [
             InputRequired(message="Se requiere al menos una cantidad"),
-            NumberRange(min=0, max=1e9),
+            NumberRange(min=1, max=1e9, message="Debe ser una cantidad entre 1 y 1e9."),
         ],
+        widget=NumberInput(min=1, max=1e9, step=1),
         description="Unidades de producto 1",
     )
     product_id_2 = SelectField("Producto 2", coerce=int, choices=pr_choices)
     quantity_2 = IntegerField(
         "Cantidad 2",
-        [NumberRange(min=0, max=1e9)],
+        [
+            NumberRange(min=1, max=1e9, message="Debe ser una cantidad entre 1 y 1e9."),
+        ],
+        widget=NumberInput(min=1, max=1e9, step=1),
         description="Unidades de producto 1",
     )
     product_id_3 = SelectField("Producto 3", coerce=int, choices=pr_choices)
     quantity_3 = IntegerField(
         "Cantidad 3",
-        [NumberRange(min=0, max=1e9)],
+        [
+            NumberRange(min=1, max=1e9, message="Debe ser una cantidad entre 1 y 1e9."),
+        ],
+        widget=NumberInput(min=1, max=1e9, step=1),
         description="Unidades de producto 1",
     )
     product_id_4 = SelectField("Producto 4", coerce=int, choices=pr_choices)
     quantity_4 = IntegerField(
         "Cantidad 4",
-        [NumberRange(min=0, max=1e9)],
+        [
+            NumberRange(min=1, max=1e9, message="Debe ser una cantidad entre 1 y 1e9."),
+        ],
+        widget=NumberInput(min=1, max=1e9, step=1),
         description="Unidades de producto 1",
     )
     product_id_5 = SelectField("Producto 5", coerce=int, choices=pr_choices)
     quantity_5 = IntegerField(
         "Cantidad 5",
-        [NumberRange(min=0, max=1e9)],
+        [
+            NumberRange(min=1, max=1e9, message="Debe ser una cantidad entre 1 y 1e9."),
+        ],
+        widget=NumberInput(min=1, max=1e9, step=1),
         description="Unidades de producto 1",
     )
     submit = SubmitField("Crear")
